9fdfdd3fb325349b049fb605a1cc3a062f2a1504
Marc Cohen <marccohen@google.com>
2015-02-10 16:32:13 -0800

    Move chown to set ownership after components are installed. Also set DISPLAY variable in .bashrc so you can run Android Studio from an ssh session.

--- scripts/install_sdk.sh
+++ scripts/install_sdk.sh
@@ -21,10 +21,6 @@ else
   echo "SDK detected at $SDK_LOC"
 fi
 
-# Set SDK Permissions
-echo "Setting SDK permissions"
-sudo chown -R vagrant: "/home/vagrant/Android"
-
 # Install platform 21
 echo y | $SDK_LOC/tools/android update sdk --no-ui --all --filter build-tools-21.1.2
 echo y | $SDK_LOC/tools/android update sdk --no-ui --all --filter platform-tools
@@ -33,3 +29,7 @@ echo y | $SDK_LOC/tools/android update sdk --no-ui --all --filter extra-android-
 echo y | $SDK_LOC/tools/android update sdk --no-ui --all --filter extra-google-google_play_services
 echo y | $SDK_LOC/tools/android update sdk --no-ui --all --filter extra-google-m2repository
 echo y | $SDK_LOC/tools/android update sdk --no-ui --all --filter extra-android-m2repository
+
+# Set SDK Permissions
+echo "Setting SDK permissions"
+sudo chown -R vagrant: "/home/vagrant/Android"
