6d53f098f7c5b22d45a5fa7dc2bacb931b38aa29
Marc Cohen <marccohen@google.com>
2015-02-12 17:09:45 -0800

    changed to restart=no when starting containers

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -1,10 +1,11 @@
 CONTAINERS=/vagrant/container_list
+RESTART=no  # May be one of no, on-failure, or always.
 
 while read container
 do
     docker run                           \
         --privileged                     \
-        --restart                        \
+        --restart=$RESTART               \
         -e DISPLAY=$DISPLAY              \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v /dev/bus/usb:/dev/bus/usb     \
