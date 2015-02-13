7a552e6482fa73d0ad03b22d267c597ca4787cdf
Johan Euphrosine <proppy@google.com>
2015-02-12 17:30:48 -0800

    script/install_docker: build android docker images

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -3,3 +3,5 @@ set -ex
 
 curl -sSL https://get.docker.com/ubuntu/ | sh
 sudo gpasswd -a vagrant docker
+docker build -t android-base /vagrant/docker/android-base
+docker build -t android-studio /vagrant/docker/android-studio
